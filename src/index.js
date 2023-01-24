df3452fe4d09e8c8363d204e47836a281a16da20
Linas Paulauskas <paulauskas.linas@gmail.com>
2023-01-24 10:17:53 +0200

    add tab switching logic

--- src/index.js
+++ src/index.js
@@ -2,16 +2,43 @@ import createHeader from './header'
 import createFooter from './footer'
 import createNavBar from './nav'
 import createHome from './home'
+import createMenu from './menu'
+import createContact from './contact'
 
 import "./reset.css"
 import "./TOP-header-footer.css"
 import "./nav.css"
 import "./home.css"
-
-console.log('test')
+import "./menu.css"
+import "./contact.css"
 
 const content = document.getElementById('content');
 content.appendChild(createHeader());
 content.appendChild(createNavBar());
 content.appendChild(createHome());
 content.appendChild(createFooter());
+
+const footer = document.querySelector("footer");
+const navButtons = document.querySelectorAll(".nav-list__item");
+const renderPage = (item) => {
+  if(item.id === "home" & !item.classList.contains("nav-list__item--is-active")){
+    item.classList.add("nav-list__item--is-active");
+    content.removeChild(footer.previousSibling);
+    content.insertBefore(createHome(), footer);
+    navButtons.forEach(button => button.id !== "home" && button.classList.remove("nav-list__item--is-active"));
+  } else if(item.id === "menu" & !item.classList.contains("nav-list__item--is-active")){
+    item.classList.add("nav-list__item--is-active");
+    content.removeChild(footer.previousSibling);
+    content.insertBefore(createMenu(), footer);
+    navButtons.forEach(button => button.id !== "menu" && button.classList.remove("nav-list__item--is-active"));
+  } else if(item.id === "contact" & !item.classList.contains("nav-list__item--is-active")){
+    item.classList.add("nav-list__item--is-active");
+    content.removeChild(footer.previousSibling);
+    content.insertBefore(createContact(), footer);
+    navButtons.forEach(button => button.id !== "menu" && button.classList.remove("nav-list__item--is-active"));
+  }
+}
+
+navButtons.forEach(button => button.addEventListener('click', () => {
+  renderPage(button)
+}))
